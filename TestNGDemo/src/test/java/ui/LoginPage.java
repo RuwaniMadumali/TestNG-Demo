e9901f0dab85caa5a96eca172b6e5b107ece1b7a
RuwaniMadumali <ruwanimadumalis@gmail.com>
2021-02-27 13:14:25 +0530

    Basic test case with priority and description

--- TestNGDemo/src/test/java/ui/LoginPage.java
+++ TestNGDemo/src/test/java/ui/LoginPage.java
@@ -1,8 +1,20 @@
 package ui;
 
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
 public class LoginPage {
 	
+	@Test(priority =1,description = "This is login test" )
+	public void loginTest() {
+		System.out.println("login testcase executed");
+	}
 	
-
+	@Test(priority =2,description = "This is logut test" )
+	public void logoutTest() {
+		System.out.println("logout test case executed");
+		
+	}
 
 }
